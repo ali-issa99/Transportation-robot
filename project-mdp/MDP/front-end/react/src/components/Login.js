334b0b17a36d17fc5061a692aca26e0a52bd3274
ali-issa99 <digitale119@hotmail.com>
2021-04-02 23:47:21 +0300

    modifie

--- project-mdp/MDP/front-end/react/src/components/Login.js
+++ project-mdp/MDP/front-end/react/src/components/Login.js
@@ -41,73 +41,7 @@ class Login extends Component {
     handleLogout() {
         this.props.logoutUser();
     }
-/* <div className="text-center">
 
-    <form className="form-signin" onSubmit={onSubmit}>
-
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-
-        <label htmlFor="inputEmail" className="sr-only">
-
-            Email address
-
-</label>
-
-        <input
-
-            type="email"
-
-            id="inputEmail"
-
-            className="form-control top-field"
-
-            placeholder="Email address"
-
-            required
-
-            autoFocus
-
-        />
-
-        <label htmlFor="inputPassword" className="sr-only">
-
-            Password
-
-</label>
-
-        <input
-
-            type="password"
-
-            id="inputPassword"
-
-            className="form-control mb-3 bottom-field"
-
-            placeholder="Password"
-
-            required
-
-        />
-
-        <button className="btn btn-lg btn-primary btn-block" type="submit">
-
-            Sign in
-
-</button>
-
-        <p className="mt-3 mb-3 text-muted text-center">
-
-            No account ? <Link to="/auth/register">Create one here</Link>
-
-        </p>
-
-    </form>
-
-</div>
-
-);
-
-}; */
 
 render() {
     return (
